d6ce897c50946d1d00c4ac51752f757b84a556f6
Davide Passerò <shardonian86@gmail.com>
2020-06-04 17:04:58 +0200

    Custom hook

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,9 +1,10 @@
-import React, {useState, useContext} from 'react'
+import React, {useContext} from 'react'
 import {Context}from '../Context'
+import useHover from '../hooks/useHover'
 
 function CartItem({item}) {
     const {removeFromCart} = useContext(Context)
-    const [hovered, setHovered] = useState(false)
+    const [hovered, ref] = useHover()
 
     const iconClassName = hovered ? "ri-delete-bin-fill" : "ri-delete-bin-line"
 
@@ -12,8 +13,7 @@ function CartItem({item}) {
             <i 
                 className={iconClassName}
                 onClick = {() => removeFromCart(item.id)}
-                onMouseEnter={() => setHovered(true)}
-                onMouseLeave={() => setHovered(false)}
+                ref={ref}
             >
             </i>
 
--- src/pages/Mobile.js
+++ src/pages/Mobile.js
@@ -1,21 +1,17 @@
-import React, { Fragment, useState, useContext } from 'react'
+import React, { Fragment, useContext } from 'react'
 import data from '../data'
 import MobileCarousel from '../components/MobileCarousel'
 import Footer from '../components/Footer'
+import useHover from '../hooks/useHover'
 
 import {Context} from '../Context'
 
 function Mobile() {
-    const [hovered, setHovered] = useState(false)
     const {cartItems, addToCart, removeFromCart} = useContext(Context)
+    const [hovered, ref] = useHover()
 
     const mobiles = data.map(mobile => (
-        <div className='card' key={mobile.id}
-             onMouseEnter = {() => setHovered(true)}
-             onMouseLeave = {() => setHovered(false)}
-            
-        >
-               
+        <div className='card' key={mobile.id}>
                 <img src= {mobile.img}  alt='Mobile phones' className='mobile-img' />
                 <h3 className='mobile-title'>{mobile.title}</h3>
                 <p className='mobile-info'>{mobile.info}</p>
@@ -23,19 +19,19 @@ function Mobile() {
         </div>
     
     ))
-    
-function cartIcon(item) {
-    const alreadyInCart = cartItems.includes(item)
-    if(alreadyInCart) {
-        return <i className="ri-shopping-cart-fill cart" onClick = {() => removeFromCart(item.id)}></i>
-    } else if(hovered) {
-        return <i className="ri-add-circle-line cart"   onClick = {() => addToCart(item)}></i>
+
+    function cartIcon(item) {
+        const alreadyInCart = cartItems.includes(item)
+        if(alreadyInCart) {
+            return <i className="ri-shopping-cart-fill cart" onClick = {() => removeFromCart(item.id)}></i>
+        } else if(hovered) {
+            return <i className="ri-add-circle-line cart"   onClick = {() => addToCart(item)}></i>
+        }
     }
-}
 
     return(
         <Fragment>
-            <main className='container'> 
+            <main className='container' ref={ref}> 
                 <h1 className='our-products-checkout'>
                     <i className="ri-smartphone-fill"></i>
                     Our Products
