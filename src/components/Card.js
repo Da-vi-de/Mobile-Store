f5bc1dc990bd6268fbefb53b19932a2951dc8ceb
Davide Passerò <shardonian86@gmail.com>
2020-06-05 11:45:19 +0200

    Refactoring

---
+++ src/components/Card.js
@@ -0,0 +1,32 @@
+import React, { useContext } from 'react'
+import useHover from '../hooks/useHover'
+
+import {Context} from '../Context'
+
+function Card({mobile}) {
+    const {cartItems, addToCart, removeFromCart} = useContext(Context)
+    const [hovered, ref] = useHover()
+
+    function cartIcon(item) {
+        const alreadyInCart = cartItems.includes(item)
+        if(alreadyInCart) {
+            return <i className="ri-shopping-cart-fill cart" onClick = {() => removeFromCart(item.id)}></i>
+        } else if(hovered) {
+            return <i className="ri-add-circle-line cart"   onClick = {() => addToCart(item)}></i>
+        }
+    }
+
+    return(
+            <main className='card' ref={ref}> 
+                <img src= {mobile.img}  alt='Mobile phones' className='mobile-img' />
+                <h3 className='mobile-title'>{mobile.title}</h3>
+                <p className='mobile-info'>{mobile.info}</p>
+                <p className='mobile-price'>{mobile.price} € {cartIcon(mobile)}</p>
+                
+            </main>
+    
+    )
+}
+
+
+export default Card
--- src/pages/Mobile.js
+++ src/pages/Mobile.js
@@ -1,46 +1,26 @@
-import React, { Fragment, useContext } from 'react'
+import React, { Fragment } from 'react'
 import data from '../data'
+import Card from '../components/Card'
 import MobileCarousel from '../components/MobileCarousel'
 import Footer from '../components/Footer'
-import useHover from '../hooks/useHover'
-
-import {Context} from '../Context'
 
 function Mobile() {
-    const {cartItems, addToCart, removeFromCart} = useContext(Context)
-    const [hovered, ref] = useHover()
-
     const mobiles = data.map(mobile => (
-        <div className='card' key={mobile.id}>
-                <img src= {mobile.img}  alt='Mobile phones' className='mobile-img' />
-                <h3 className='mobile-title'>{mobile.title}</h3>
-                <p className='mobile-info'>{mobile.info}</p>
-                <p className='mobile-price'>{mobile.price} € {cartIcon(mobile)}</p>
-        </div>
-    
-    ))
+        <Card  key={mobile.id} mobile={mobile} />
 
-    function cartIcon(item) {
-        const alreadyInCart = cartItems.includes(item)
-        if(alreadyInCart) {
-            return <i className="ri-shopping-cart-fill cart" onClick = {() => removeFromCart(item.id)}></i>
-        } else if(hovered) {
-            return <i className="ri-add-circle-line cart"   onClick = {() => addToCart(item)}></i>
-        }
-    }
+    ))
 
     return(
         <Fragment>
-            <main className='container' ref={ref}> 
+            <main className='container'>
                 <h1 className='our-products-checkout'>
-                    <i className="ri-smartphone-fill"></i>
-                    Our Products
-                </h1>
+                        <i className="ri-smartphone-fill"></i>
+                        Our Products
+                    </h1>
 
-                <MobileCarousel />
-            
-                {mobiles}
-                
+                    <MobileCarousel />
+
+                    {mobiles}
             </main>
 
             <Footer />
